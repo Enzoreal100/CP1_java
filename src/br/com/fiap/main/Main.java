2dec122930d8d928b1230f3f68ad62e456e9b4f4
Laboratório FIAP <labsfiap@fiap.com.br>
2025-03-27 10:52:32 -0300

    String resolvida e comentarios

--- src/br/com/fiap/main/Main.java
+++ src/br/com/fiap/main/Main.java
@@ -6,11 +6,13 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        // Instanciar novo objeto com base na DespesaFamiliar
         DespesaFamiliar novoCalculo = new DespesaFamiliar();
+        // Instanciar objeto da classe Scan
         Scanner scan = new Scanner(System.in);
-        String resposta;
 
         try {
+            // pegar inputs do usuário e implementar nos atributos do objeto novoCalculo instanciado da classe DespesaFamiliar
             System.out.println("Digite o total que a família recebe no mês: ");
             novoCalculo.rendaFamiliar = scan.nextDouble();
             System.out.println("Digite o número de moradores da casa: ");
@@ -21,9 +23,11 @@ public class Main {
             novoCalculo.gastoComInternet = scan.nextDouble();
             System.out.println("Digite o valor da mensalidade da academia por pessoa: ");
             novoCalculo.valorMensalidadeDaAcademia = scan.nextDouble();
-            System.out.println(novoCalculo.valorMensalidadeDaAcademia);
-            System.out.printf("Dados Fornecidos:\nRenda Familiar Bruta: %.3f\nNúmero de moradores da casa: %d\nGasto mensal da casa com luz: %f.3f\nGasto mensal da casa com internet: %f.3f\nGasto mensal da casa com academia: %f.3f\n Total em gastos mensais: %f.3f\nRenda Familiar Líquida: %f.3f", novoCalculo.rendaFamiliar, novoCalculo.numeroDeMoradores, novoCalculo.gastoComLuz, novoCalculo.gastoComInternet, novoCalculo.valorMensalidadeDaAcademia, novoCalculo.calcularTotalDeDespesas(), novoCalculo.calcularRendaFamiliarLiquida());
-        } catch (Exception e){
+            // print dos dados em Format
+            System.out.printf("Dados Fornecidos:\nRenda Familiar Bruta: %.3f\nNúmero de moradores da casa: %d\nGasto mensal da casa com luz: %.3f\nGasto mensal da casa com internet: %.3f\nGasto mensal da casa com academia: %.3f\n Total em gastos mensais: %.3f\nRenda Familiar Líquida: %.3f", novoCalculo.rendaFamiliar, novoCalculo.numeroDeMoradores, novoCalculo.gastoComLuz, novoCalculo.gastoComInternet, novoCalculo.valorMensalidadeDaAcademia, novoCalculo.calcularTotalDeDespesas(), novoCalculo.calcularRendaFamiliarLiquida());
+
+//  Se tiver erro, vai vir pra cá e printa a mensagem de erro
+        } catch (Exception e) {
             System.out.println("Formato de dado inválido");
         }
     }
